799dfabff929fd476312fbf103b415ea8bc4af46
Farid Hussain <apple@Farids-Macbook-pro.local>
2023-07-04 07:13:44 +0500

    Add imports

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-def reverse_array(array):
-    j = len(arr) - 1
-    for i in range(len(array)):
-        if j > i:
-            temp = array[i]
-            array[i] = array[j]
-            array[j] = temp
-            j -= 1
-    print(array)
-
-
-arr = [2, 8, 7, 6, 5]
-reverse_array(arr)
+import random
